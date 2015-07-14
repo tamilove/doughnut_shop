53836dabce5ac27d087a440930f362da403390f2
Tami Love <thetamilove@gmail.com>
2015-07-14 15:19:20 -0700

    second version of my doughnut shop with Emre's help. Still don't know what I am doing.

--- shop.js
+++ shop.js
@@ -5,41 +5,48 @@ var Shop = function(local, minCust, maxCust, averagePerCust, hourStart, hourEnd)
     this.averagePerCust = averagePerCust;
     this.hourStart = hourStart;
     this.hourEnd = hourEnd;
+    this.hoursOpen = this.hourEnd - this.hoursOpen;
+    this.hourly = [];
+    this.totalDoughnuts = 0;
   };
 
 Shop.prototype.display = function() { // You need this section to print things onto your document.
-  var popCorn = document.createElement ('td');
-  var totalHours = this.hourEnd - this.hourStart;
-  popCorn.innerHTML =  ;
-  return popCorn;
+  var createTR = document.createElement('tr');
+  var table = document.getElementById('doughnut_table');
+  var total = 0;
+  createTR.innerHTML = this.local;
+  table.appendChild(createTR);
+
+  var dough = this.doughnutsPerDay();
+  for (var i = 0; i < dough.length; i++) {
+    var td = document.createElement('td');
+    td.innerHTML = dough[i];
+    createTR.appendChild(td);
+    total += dough[i];
+  }
+  var totalTd = document.createElement('totalTd');
+  totalTd.innerHTML = total;
+  createTR.appendChild(totalTd);
 };
 
-
-  Shop.prototype.getRandomCust = function(minCust, maxCust) {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-  };
-  Shop.prototype.hoursOpen = function(hourStart, hourEnd) {
-    return this.hourEnd - this.hourStart;
-  };
-
-  Shop.prototype.randomDoughnuts = function(averagePerCust, getRandomCust) {
-    return (this.getRandomCust() * this.averagePerCust);
-  };
-
+//This is here to create a "SUM" of the all radnom customer weighted by average doughnuts per cust.
   Shop.prototype.doughnutsPerDay = function(){
-    var total = 0;
-    for (var i = 0; i < this.hoursOpen; i++);
-      total += this.randomDoughbuts
-  }:
+    var hourly = [];
+    for (i = 0; i < 12; i++){
+      hourly.push((Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust) * this.averagePerCust);
+    }
+    return hourly;
+  };
 
   var downTown = new Shop("Downtown", 8, 43, 4.5, 7, 18);
   var capitolHill = new Shop("Capital Hill", 4, 37, 2.0, 7, 18);
-  var southLakeUnion = new Shop("South Lake Union", 9, 23, 6.3, 7, 18);
-  var wedgewood = new Shop("Wedgewood", 2, 28, 1.2, 7, 18);
-  var ballard = new Shop("Ballard", 8, 58, 3.7, 7, 18);
+  var southLakeUnion = new Shop("South Lake Union", 9, 23, 6.25, 7, 18);
+  var wedgewood = new Shop("Wedgewood", 2, 28, 1.25, 7, 18);
+  var ballard = new Shop("Ballard", 8, 58, 3.75, 7, 18);
 
-  var main = document.getElementById('content');
-  main.appendChild(downTown.display());
-
-
+downTown.display();
+capitolHill.display();
+southLakeUnion.display();
+wedgewood.display();
+ballard.display();
 
